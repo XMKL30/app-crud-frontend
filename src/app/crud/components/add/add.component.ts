9ff58ca862903b502ff857847ac1509b7301cc90
XMKL30 <mikjaeljeandet_2003@hotmail.com>
2023-12-18 23:37:55 -0500

    Implementación de funcionalidad crear salesperson

--- src/app/crud/components/add/add.component.ts
+++ src/app/crud/components/add/add.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CrudService } from '../../services/crud.service';
+import { Router } from '@angular/router';
+import { Salesperson } from '../../models/salesperson';
 
 @Component({
   selector: 'crud-add',
@@ -6,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './add.component.css'
 })
 export class AddComponent {
+
+  public salesPerson: Salesperson = new Salesperson();
+
+  constructor(private crudService: CrudService, private route: Router) {}
+
+  createSalesperson() {
+    console.log(this.salesPerson);
+    this.crudService.insert(this.salesPerson).subscribe(() => {});
+    this.route.navigateByUrl('salesperson')
+  }
 
 }
--- src/app/crud/crud.module.ts
+++ src/app/crud/crud.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from '../app-routing.module';
+
+import { MatTableModule } from '@angular/material/table' 
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
 import { MainComponent } from './pages/main/main.component';
 import { ListComponent } from './components/list/list.component';
 import { AddComponent } from './components/add/add.component';
-import { MatTableModule } from '@angular/material/table' 
-import { HttpClientModule } from '@angular/common/http';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { AppRoutingModule } from '../app-routing.module';
-import { MatButtonModule } from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -24,7 +27,9 @@ import {MatIconModule} from '@angular/material/icon';
     MatPaginatorModule,
     AppRoutingModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatInputModule,
+    FormsModule
   ],
   exports: [
     MainComponent
